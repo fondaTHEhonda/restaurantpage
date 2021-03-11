1d5bf2421d354cb46c4b20e03db06eb13491a4e4
fondaTHEhonda <casatchell15@gmail.com>
2021-03-10 20:06:20 -0500

    more styling changes

--- src/menuPage.js
+++ src/menuPage.js
@@ -8,9 +8,19 @@ let cookies = [
     {Type: "Oatmeal Raisin", Description: "Brown sugar, cinnamon, and raisins. A simple but delicious cookie", Price: "$1.99"},
     {Type: "Cookie's Special", Description: "A cat friendly treat infused with catnip", Price: "$2.49"}
 ];
+
+let pageContainer = document.createElement('div');
+pageContainer.id = "page-container";
 let tableDiv = document.createElement("div");
+tableDiv.id = "table-div";
 let menuTable = document.createElement("table");
+menuTable.id = "table-display";
 let data = Object.keys(cookies[0]);
+
+let menu = document.createElement('p');
+menu.textContent = "Menu";
+menu.id = "menu-p";
+pageContainer.appendChild(menu);
 
 function createTableHead(table, data) {
 
@@ -18,6 +28,7 @@ function createTableHead(table, data) {
     let row = thead.insertRow();
     for(let key of data) {
         let th = document.createElement("th");
+        th.id = "table-head";
         let text = document.createTextNode(key);
         th.appendChild(text);
         row.appendChild(th);
@@ -34,8 +45,10 @@ function createTable(table, data) {
             cell.appendChild(cellText);
         }
     }
+    
     tableDiv.appendChild(menuTable);
-    return document.getElementById('content').appendChild(tableDiv);
+    pageContainer.appendChild(tableDiv);
+    return document.getElementById('content').appendChild(pageContainer);
 }
 
 const menuContent = function() {
@@ -45,13 +58,18 @@ const menuContent = function() {
     createTableHead(menuTable, data);
     
     let specials = document.createElement("div");
-    let info = document.createElement("h3");
+    specials.id = "special-container";
+    let info = document.createElement("p");
+    info.id = "special-title";
     let specialInfo = document.createElement('ul');
     info.textContent = "See our specials!"
 
     let dailyDeal = document.createElement('li');
+    dailyDeal.classList.add("special-list");
     let occupationDeal = document.createElement('li')
+    occupationDeal.classList.add("special-list");
     let bogo = document.createElement('li');
+    bogo.classList.add("special-list");
 
     dailyDeal.textContent = "25% off on all Chungus packs on Wednesdays!"
     occupationDeal.textContent = "Veterans, teachers, first responders, and nurses get 10% off every cookie!"
@@ -64,8 +82,11 @@ const menuContent = function() {
     specialInfo.appendChild(bogo);
 
     specials.appendChild(specialInfo);
+    pageContainer.appendChild(specials);
 
-    return document.getElementById("content").appendChild(specials);
+
+
+    return document.getElementById("content").appendChild(pageContainer);
 
     
 }
